0db55016fee2f14599acd9d3c986a5e30b0d2313
React <react@blackcapit.com>
2025-11-20 17:20:53 +0530

    Progress Dashboard Added

--- src/pages/AirliftAnimations.jsx
+++ src/pages/AirliftAnimations.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Animations from "@/components/custom/Animations";
 import ProgressBar from "@/components/custom/ProgressBar";
+import ProgressDashboard from "@/components/custom/ProgressDashboard";
 
 const AirliftAnimations = () => {
   return (
@@ -8,6 +9,7 @@ const AirliftAnimations = () => {
       <div className="p-5 space-y-5">
         <Animations />
         <ProgressBar />
+        <ProgressDashboard />
       </div>
     </>
   );
